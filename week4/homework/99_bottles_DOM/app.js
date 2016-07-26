21e424702f3e116e7ad1b3fa1de69f27943fb029
natesulat <nate.sulat@gmail.com>
2016-07-25 20:53:26 -0400

    99 bottles of beer are on the wall and ready for drinking

--- week4/homework/99_bottles_DOM/app.js
+++ week4/homework/99_bottles_DOM/app.js
@@ -1,10 +1,20 @@
-$('#makeitwork').click(function () {
+$('#makeitwork').click(function (e) {
+    // I mean... just to be safe, or whatever
+    e.preventDefault();
     for (i = 99; i >= 0; i--) {
-        if (i !== 0) {
-            $('.masterlist').append('<li>' + i + ' bottles of beer on the wall, '
-                i + ' bottles of beer! You take one down, pass it around, ' + i + ' bottles of beer on the wall!' + '</li>');
-        } else {
-            $('.masterlist').append('There aren\'t any bottles of beer on the wall, there aren\'t any bottles of beer! You can\'t take it down or pass it around, because you ran out of money on the previous 99 beers!');
+        // lyric as variable so that I don't have to type it twice, and so I can do string replacement later
+        var lyric = '<li>' + i + ' bottles of beer on the wall, ' + i + ' bottles of beer! You take one down, pass it around, ' + i + ' bottles of beer on the wall!' + '</li>';
+        // One bottle ain't gonna be plural
+        if (i === 1) {
+            $('#masterlist').append(lyric.replace(/\bbottles\b/ig, 'bottle'));
+        } 
+        // The others are
+        else if (i !== 0) {
+            $('#masterlist').append(lyric);
+        } 
+        // Once there are no more bottles on the wall
+        else {
+            $('#masterlist').append('<li>There aren\'t any bottles of beer on the wall, there aren\'t any bottles of beer! You can\'t take it down or pass it around, because you ran out of money on the previous 99 beers!');
         }
     }
 });
