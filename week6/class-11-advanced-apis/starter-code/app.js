3292b4a1f4ce904946544501cc97734ce5f16790
natesulat <nate.sulat@ogilvy.com>
2016-08-04 21:08:08 -0400

    Solution codes

--- week6/class-11-advanced-apis/starter-code/app.js
+++ week6/class-11-advanced-apis/starter-code/app.js
@@ -1,5 +1,11 @@
 $(function() {
   // DOM is now ready
-
+    _500px.init({
+        sdk_key: '36734947feb3eb47fbe53ee816a069bfd9542f3b'
+    });
+    
+    $('#login').click(function() {
+        _500px.login();
+    });
 
 });
