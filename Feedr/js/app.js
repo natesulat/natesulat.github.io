b36d964acf5cae39f8b7b9e57cbcb1fec7cd46f1
natesulat <nate.sulat@ogilvy.com>
2016-08-10 15:30:10 -0400

    Bower stuff added, plus Feedr updates

--- Feedr/js/app.js
+++ Feedr/js/app.js
@@ -2,7 +2,42 @@
   Please add all Javascript code to this file.
 */
 
+//var sources = ['Digg','Mashable','Reddit'];
+//
+//var listing = models.listing;
+//listing(sources, '#search');
+
 
 //sample helper function
-var help = models
-help.hello("world")
+var feedObj = {},
+    articles = [];
+
+// DIGG AJAX
+$.ajax({
+    url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
+    type: 'GET',
+    success: function (res) {
+        var diggFeed = res.data.feed;
+        $.each(diggFeed, function(i, t) {
+            var $x = diggFeed[i],
+                obj = {},
+                tags = [];
+            $.each($x.content.tags, function(j, u) {
+                tags.push($x.content.tags[j].slug);
+            })
+            obj.title = $x.content.title;
+            obj.excerpt = $x.content.description;
+            obj.url = $x.content.url;
+            obj.juju = $x.digg_score;
+            obj.tags = tags;
+            obj.img = $x.content.media.images[0].original_url;
+            obj.pubDate = $x.date_published;
+            articles.push(obj); 
+        });
+    },
+    error: function() {
+        console.log('digg error');
+    }
+});
+
+console.log(articles);
--- Feedr/js/helperUtils.js
+++ Feedr/js/helperUtils.js
@@ -1,5 +1,11 @@
 var models = {};
 
-models.hello = function(word) {
-  console.log("Hello", word)
+models.hello = function (word) {
+    console.log("Hello", word)
+}
+
+models.listing = function (list,target) {
+    for (i = 0; i < list.length; i++) {
+        $(target).append('<li>' + list[i] + '</li>');
+    }
 }
